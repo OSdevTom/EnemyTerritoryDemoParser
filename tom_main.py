9798dd15748cf828d66349ea78b37e146c8d48b0
sbxtot <tom.stock@hotmail.com>
2021-12-31 16:44:31 +0100

    first attempt to train a svdd, stopped because we need to remove the NaN value from the datafram

--- tom_main.py
+++ tom_main.py
@@ -6,14 +6,31 @@ dir_name = 'C:\demos'
 import pandas as pd
 import numpy as np
 import data_preparation
+from basesvdd import BaseSVDD
+
+###demos are organized by players###
+###each player has a folder in which all of their demos are stored###
+###so we loop in each folder to read each demos###
+###and we append each demo from each player into a large ABT###
+###df_appended is out ABT = Analytical Based Table###
 
 df_appended = pd.DataFrame()
 
 for dir in os.listdir(dir_name):
     for filename in os.listdir(os.path.join(dir_name, dir)):
-        df = pd.read_csv(os.path.join(dir_name, dir, filename), sep=';')
+        df = pd.read_csv(os.path.join(dir_name, dir, filename), sep=';', nrows=300)
         df_output = data_preparation.demo_to_dataframe_parser(df)
         ##print(df_output)
         df_appended = df_appended.append(df_output, ignore_index=True)
         ###print(df_appended)
-        df_appended.describe()
+
+# svdd object using rbf kernel
+
+df_appended = df_appended.drop(columns=["player","timestamp","shooting","hit","gun","crouch"])
+training_data = df_appended.to_numpy()
+
+svdd = BaseSVDD(C=0.9, gamma=0.3, kernel='rbf', display='on')
+
+# fit the SVDD model
+svdd.fit(training_data)
+
