cba6f9f352a9f80d4d7fadbce5bd4a9ad3977776
sbxtot <tom.stock@hotmail.com>
2021-12-31 12:42:05 +0100

    iterating within the main directory and subdirectories

--- tom_main.py
+++ tom_main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import os
-dir_name = 'C:\demos\Wut'
+dir_name = 'C:\demos'
 
 # import pandas module
 import pandas as pd
@@ -9,9 +9,11 @@ import data_preparation
 
 df_appended = pd.DataFrame()
 
-for filename in os.listdir(dir_name):
-    df = pd.read_csv(os.path.join(dir_name, filename), sep=';')
-    df_output = data_preparation.demo_to_dataframe_parser(df)
-    ##print(df_output)
-    df_appended = df_appended.append(df_output, ignore_index=True)
-    print(df_appended)
+for dir in os.listdir(dir_name):
+    for filename in os.listdir(os.path.join(dir_name, dir)):
+        df = pd.read_csv(os.path.join(dir_name, dir, filename), sep=';')
+        df_output = data_preparation.demo_to_dataframe_parser(df)
+        ##print(df_output)
+        df_appended = df_appended.append(df_output, ignore_index=True)
+        ###print(df_appended)
+        df_appended.describe()
