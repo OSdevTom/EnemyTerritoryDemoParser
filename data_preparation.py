c13a75c16a05530da78110f6011d786b51a671f4
OSdevTom <OSdevTom>
2025-09-22 12:27:07 +0000

    nobranchupdate

--- data_preparation.py
+++ data_preparation.py
@@ -20,6 +20,7 @@ def demo_to_dataframe_parser(df):
     df["last_5_velocity_x_sum"] = df.velocity_player_x.rolling(5).sum().shift(1)
     df["last_5_velocity_x_mean"] = df.velocity_player_x.rolling(5).mean().shift(1)
     df["last_5_velocity_x_var"] = df.velocity_player_x.rolling(5).var().shift(1)
+    test if branch deleted
 
     df["last_5_velocity_y_sum"] = df.velocity_player_y.rolling(5).sum().shift(1)
     df["last_5_velocity_y_mean"] = df.velocity_player_y.rolling(5).mean().shift(1)
